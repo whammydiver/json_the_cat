d5e808672640de892865d39bdb8d5c1afa278479
Paul Taylor <paul@whammydiver.com>
2022-06-25 18:47:24 -0400

    first commit breedFetcher.js

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const breed = process.argv.slice(2);
+const searchTerm = breed[0].slice(0, 3);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, (error, response, body) => {
+
+  if (error) {
+    console.log(`Request error. ${error}`);
+    return;
+  }
+
+  const [data] = JSON.parse(body);
+
+  if (!data) {
+    console.log('Breed not found');
+    return;
+  }
+
+  console.log(data.description);
+
+});
